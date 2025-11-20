7ef30ea441640340c4ed2ff768ee8a593bbf6b2f
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2025-11-20 15:17:27 +0530

    fix: admin flow view permission

--- internal/core/rbac.go
+++ internal/core/rbac.go
@@ -34,6 +34,8 @@ func (c *Core) InitializeRBACPolicies() error {
 	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceFlow), string(models.RBACActionCreate))
 	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceFlow), string(models.RBACActionUpdate))
 	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceFlow), string(models.RBACActionDelete))
+	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceFlow), string(models.RBACActionView))
+	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceFlow), string(models.RBACActionExecute))
 	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceNode), string(models.RBACActionView))
 	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceNode), string(models.RBACActionCreate))
 	c.enforcer.AddPolicy("role:admin", "*", string(models.ResourceNode), string(models.RBACActionUpdate))
